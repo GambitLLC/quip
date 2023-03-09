d00b7f1e4e23886284d20fdfe4210eb7dcf5bee3
Randolph Xia <xiarandolph@gmail.com>
2023-03-09 16:24:20 -0500

    Update socket spec to support different clients in the future

--- libs/socket.io-server/src/lib/socket.spec.ts
+++ libs/socket.io-server/src/lib/socket.spec.ts
@@ -21,8 +21,7 @@ import {
 } from '@grpc/grpc-js/build/src/server-call';
 
 process.env.ALLOW_CONFIG_MUTATIONS = 'true';
-import importFresh from 'import-fresh';
-import { IConfig } from 'config';
+import config from 'config';
 
 interface Call {
   player: string;
@@ -53,8 +52,9 @@ function trackCall(
 
 function gotCall(call: Call): boolean {
   return (
-    calls.find((elem) => JSON.stringify(elem) === JSON.stringify(call)) !==
-    undefined
+    calls.find(
+      (elem) => elem.player === call.player && elem.request === call.request
+    ) !== undefined
   );
 }
 
@@ -92,46 +92,71 @@ const mockMatchmaker: MatchmakerServer = {
 };
 
 describe('socket listener', () => {
-  let config: IConfig;
   let io: SocketServer,
     grpc: grpcServer,
     // serverSocket: ServerSocket<ClientToServerEvents, ServerToClientEvents>,
     clientSocket: ClientSocket<ServerToClientEvents, ClientToServerEvents>;
 
+  async function newClient(
+    player: string
+  ): Promise<ClientSocket<ServerToClientEvents, ClientToServerEvents>> {
+    const host = config.get('api.socket_io-server.hostname');
+    const port = config.get('api.socket_io-server.port');
+
+    // TODO: add authentication as socket option
+    const client = Client(`http://${host}:${port}`, {
+      transports: ['websocket'],
+    });
+
+    await new Promise<void>((resolve, reject) => {
+      client.on('connect', resolve);
+      client.on('connect_error', reject);
+    });
+
+    return client;
+  }
+
   beforeAll(async () => {
-    const wait = new Promise((resolve) => {
+    // wait for matchmaker service to start so that config is set with proper values
+    await new Promise<void>((resolve, reject) => {
       grpc = new grpcServer();
       grpc.addService(MatchmakerService, mockMatchmaker);
       grpc.bindAsync(
         'localhost:0',
         ServerCredentials.createInsecure(),
         (err, port) => {
-          expect(err).toBeNull;
-          process.env.NODE_CONFIG = JSON.stringify({
+          if (err) {
+            reject(err);
+            return;
+          }
+
+          config.util.extendDeep(config, {
             api: {
               matchmaker: { hostname: 'localhost', port: port },
             },
           });
 
-          config = importFresh('config');
           grpc.start();
-          resolve(null);
+          resolve();
         }
       );
     });
 
-    await wait;
-    return new Promise<void>((done) => {
+    // wait for socket server to listen
+    await new Promise<void>((resolve) => {
       const httpServer = createServer();
       io = wrapServer(config, httpServer);
 
-      httpServer.listen(() => {
+      httpServer.listen(async () => {
         const { port } = httpServer.address() as AddressInfo;
-        clientSocket = Client(`http://localhost:${port}`);
-        // io.on('connection', (socket) => {
-        //   serverSocket = socket;
-        // });
-        clientSocket.on('connect', done);
+        config.util.extendDeep(config, {
+          api: {
+            'socket_io-server': { hostname: 'localhost', port: port },
+          },
+        });
+
+        clientSocket = await newClient('asdsa');
+        resolve();
       });
     });
   });
@@ -143,6 +168,16 @@ describe('socket listener', () => {
 
   test('should receive status', (done) => {
     clientSocket.emit('getStatus', (err) => {
+      if (
+        !gotCall({
+          player: 'asdsa',
+          request: 'getStatus',
+        })
+      ) {
+        done('mock matchmaker did not receive getStatus request');
+        return;
+      }
+
       done(err);
     });
   });
