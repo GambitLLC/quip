49114e15ab79bcf65c81ab6a683e31d389889483
Randolph Xia <xiarandolph@gmail.com>
2023-04-13 16:08:47 -0400

    Add matchmaker-backend app

--- apps/matchmaker-e2e/setup.go
+++ apps/matchmaker-e2e/setup.go
@@ -115,8 +115,10 @@ func createE2EConfig(redisPort, agonesPort string) error {
 	// spin up matchmaker services on another port as well
 	cfg.Set("matchmaker.frontend.hostname", "localhost")
 	cfg.Set("matchmaker.frontend.port", 50496)
+	cfg.Set("matchmaker.backend.hostname", "localhost")
+	cfg.Set("matchmaker.backend.port", 50495)
 	cfg.Set("matchmaker.matchfunction.hostname", "localhost")
-	cfg.Set("matchmaker.matchfunction.port", 50495)
+	cfg.Set("matchmaker.matchfunction.port", 50490)
 
 	return cfg.WriteConfig()
 }
