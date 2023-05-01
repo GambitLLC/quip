170f2f632fb8a8aa5822f75f4a556cea16be8825
bendgk <benkosten@gmail.com>
2023-05-01 14:41:18 -0400

    finally scalable avatars

--- apps/quip-web/components/util/Avatar/playful_avatars/mouth/Normal_Thin.vue
+++ apps/quip-web/components/util/Avatar/playful_avatars/mouth/Normal_Thin.vue
@@ -3,7 +3,7 @@
 
 <template>
 <svg width="30" height="16" viewBox="0 0 30 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M25.5 4.5C21.1 7.1 14.1 5.5 9 2" stroke="#FEE4BF" stroke-width="2.4681" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M25.5 4.5C21.1 7.1 14.1 5.5 9 2" stroke="#423232" stroke-width="2.4681" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
 </template>
 
--- apps/quip-web/pages/test.vue
+++ apps/quip-web/pages/test.vue
@@ -1,20 +1,135 @@
 <script setup lang="ts">
 import Avatar from "~/components/util/Avatar/Avatar.vue";
+import {accessories, eyes, faces, hairs, mouths, outfits} from "~/components/util/Avatar/types";
 </script>
 
 <template>
-  <div class="w-100 h-100 d-flex align-center justify-center">
-    <Avatar
-      :size="1300"
-      color="bg-green-light"
-      accessory="Rounded_Glasses"
-      eye="Normal"
-      face="Light"
-      hair="03"
-      mouth="Cute"
-      outfit="20"
-    />
+  <div class="pa-16">
+    <div class="d-flex">
+      <div>
+        <h1 class="py-8">
+          Jon
+        </h1>
+        <Avatar eye="Normal" face="Light" mouth="Open_Tooth" outfit="03" size="500" hair="07" color="bg-green-light"/>
+      </div>
+      <div>
+        <h1 class="py-8">
+          Alex
+        </h1>
+        <Avatar accessory="Rounded_Glasses" eye="Normal" face="Fair" mouth="Eat" outfit="20" size="500" hair="03" color="bg-green-light"/>
+      </div>
+      <div>
+        <h1 class="py-8">
+          Randolph
+        </h1>
+        <Avatar accessory="Earphone" eye="Cynic" face="Light" mouth="Hate" outfit="06" size="500" hair="16" color="bg-green-light"/>
+      </div>
+      <div>
+        <h1 class="py-8">
+          Ben
+        </h1>
+        <Avatar eye="Thin" face="Light" mouth="Open_Mouth" outfit="01" size="500" hair="08" color="bg-green-light"/>
+      </div>
+    </div>
   </div>
+<!--  <div class="w-100 h-100">-->
+<!--    <h1 class="py-8">Accessories:</h1>-->
+<!--    <div class="d-flex flex-wrap">-->
+<!--      <div class="d-flex pa-2 w-25" v-for="a in accessories">-->
+<!--        <Avatar-->
+<!--          :size="295"-->
+<!--          color="bg-green-light"-->
+<!--          :accessory="a"-->
+<!--          eye="Normal"-->
+<!--          face="Light"-->
+<!--          hair="23"-->
+<!--          mouth="Cute"-->
+<!--          outfit="01"-->
+<!--        />-->
+<!--        <h3>{{a}}</h3>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--    <h1 class="py-8">Eyes:</h1>-->
+<!--    <div class="d-flex flex-wrap">-->
+<!--      <div class="d-flex pa-2 w-25" v-for="e in eyes">-->
+<!--        <Avatar-->
+<!--          :size="295"-->
+<!--          color="bg-green-light"-->
+<!--          accessory="Simple_Earring"-->
+<!--          :eye="e"-->
+<!--          face="Light"-->
+<!--          hair="23"-->
+<!--          mouth="Cute"-->
+<!--          outfit="01"-->
+<!--        />-->
+<!--        <h3>{{e}}</h3>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--    <h1 class="py-8">Faces:</h1>-->
+<!--    <div class="d-flex flex-wrap">-->
+<!--      <div class="d-flex pa-2 w-25" v-for="f in faces">-->
+<!--        <Avatar-->
+<!--          :size="295"-->
+<!--          color="bg-green-light"-->
+<!--          accessory="Simple_Earring"-->
+<!--          eye="Normal"-->
+<!--          :face="f"-->
+<!--          hair="23"-->
+<!--          mouth="Cute"-->
+<!--          outfit="01"-->
+<!--        />-->
+<!--        <h3>{{f}}</h3>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--    <h1 class="py-8">Hairs:</h1>-->
+<!--    <div class="d-flex flex-wrap">-->
+<!--      <div class="d-flex pa-2 w-25" v-for="h in hairs">-->
+<!--        <Avatar-->
+<!--          :size="295"-->
+<!--          color="bg-green-light"-->
+<!--          accessory="Simple_Earring"-->
+<!--          eye="Normal"-->
+<!--          face="Light"-->
+<!--          :hair="h"-->
+<!--          mouth="Cute"-->
+<!--          outfit="01"-->
+<!--        />-->
+<!--        <h3>{{h}}</h3>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--    <h1 class="py-8">Mouths:</h1>-->
+<!--    <div class="d-flex flex-wrap">-->
+<!--      <div class="d-flex pa-2 w-25" v-for="m in mouths">-->
+<!--        <Avatar-->
+<!--          :size="295"-->
+<!--          color="bg-green-light"-->
+<!--          accessory="Simple_Earring"-->
+<!--          eye="Normal"-->
+<!--          face="Light"-->
+<!--          hair="23"-->
+<!--          :mouth="m"-->
+<!--          outfit="01"-->
+<!--        />-->
+<!--        <h3>{{m}}</h3>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--    <h1 class="py-8">Outfits:</h1>-->
+<!--    <div class="d-flex flex-wrap">-->
+<!--      <div class="d-flex pa-2 w-25" v-for="o in outfits">-->
+<!--        <Avatar-->
+<!--          :size="295"-->
+<!--          color="bg-green-light"-->
+<!--          accessory="Simple_Earring"-->
+<!--          eye="Normal"-->
+<!--          face="Light"-->
+<!--          hair="03"-->
+<!--          mouth="Cute"-->
+<!--          :outfit="o"-->
+<!--        />-->
+<!--        <h3>{{o}}</h3>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--  </div>-->
 </template>
 
 <style scoped lang="scss">
