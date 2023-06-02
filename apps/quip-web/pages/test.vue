e62675083bd14e16b24bb46f71340a6981028f08
bendgk <benkosten@gmail.com>
2023-06-02 14:09:47 -0400

    + added logout button to test

--- apps/quip-web/pages/test.vue
+++ apps/quip-web/pages/test.vue
@@ -3,8 +3,14 @@ import Match from "~/components/util/Match.vue";
 import QuipInput from "~/components/util/QuipInput.vue";
 const a = ref(0)
 
+const {$logout} = useNuxtApp()
+
 function test() {
   $fetch('/api/login', { method: 'post', body: { test: 123 } })
+}
+
+async function logout() {
+  await $logout()
 }
 </script>
 
@@ -12,6 +18,7 @@ function test() {
   <div class="w-100 h-100 d-flex flex-column align-center justify-center">
 <!--    <AvatarCreator/>-->
     <div @click="test()">Click me!</div>
+    <div v-ripple @click="logout()">Logout</div>
   </div>
 </template>
 
