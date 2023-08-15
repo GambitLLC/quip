8033a0b2e63be89c59764c525f64f58ce445ba88
bendgk <benkosten@gmail.com>
2023-08-15 16:08:38 -0400

    + add ripple to balance + also use the actual balance value

--- libs/native-ui/src/lib/crypto/Balance.tsx
+++ libs/native-ui/src/lib/crypto/Balance.tsx
@@ -7,6 +7,8 @@ import { typography } from "../styles/Typography";
 import { useCrypto } from "../context/CryptoContext";
 import { useTicker } from "../context/TickerContext";
 import { TouchableRipple } from "react-native-paper";
+import { useMemo } from "react";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 interface BalanceProps {
   onPress: () => void
@@ -14,13 +16,16 @@ interface BalanceProps {
 
 export function Balance(props: BalanceProps) {
   const crypto = useCrypto()
+  const {usdPrice} = useTicker()
+
+  const usdBal = useMemo(() => ((crypto.balance ?? 0) * usdPrice).toFixed(2), [crypto.balance, usdPrice])
 
   return (
     <TouchableRipple borderless onPress={props.onPress} style={[styles.balance]}>
-      <View style={[styles.balanceRow, p('a', 2), p('r', 4)]}>
-        <Sol style={styles.icon}/>
-        <Text style={[typography.p3, m('l', 2)]}>
-          {crypto.balance} SOL
+      <View style={[styles.balanceRow, p('a', 2)]}>
+        <FontAwesome style={styles.usdIcon} color={theme.colors.s1} size={14} name="usd"/>
+        <Text style={[typography.p3, styles.text, m('r', 1)]}>
+          {usdBal} USD
         </Text>
       </View>
     </TouchableRipple>
@@ -37,19 +42,19 @@ const styles = StyleSheet.create({
     borderStyle: "solid",
     borderColor: theme.colors.s3
   },
+  usdIcon: {
+    marginBottom: 1,
+    marginRight: 4,
+    color: theme.colors.s1
+  },
+  text: {
+    color: theme.colors.s1,
+  },
   balanceRow: {
     display: "flex",
     flexDirection: "row",
     alignItems: "center",
   },
-  icon: {
-    color: theme.colors.s3,
-    width: 20,
-    height: 20,
-  },
-  text: {
-
-  }
 })
 
 export default Balance
