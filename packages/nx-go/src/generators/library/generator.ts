2a59717c9185c3436878e658c15811ea0585a2ec
Randolph Xia <xiarandolph@gmail.com>
2024-01-29 18:31:25 -0500

    Create nx-go plugin

---
+++ packages/nx-go/src/generators/library/generator.ts
@@ -0,0 +1,34 @@
+import {
+  addProjectConfiguration,
+  formatFiles,
+  generateFiles,
+  names,
+  Tree,
+} from '@nx/devkit';
+import * as path from 'path';
+import { LibraryGeneratorSchema } from './schema';
+
+export async function libraryGenerator(
+  tree: Tree,
+  options: LibraryGeneratorSchema,
+) {
+  const projectDirectory = options.directory || 'internal';
+  const projectRoot = `${projectDirectory}/${options.name}`;
+  addProjectConfiguration(tree, options.name, {
+    root: projectRoot,
+    projectType: 'library',
+    sourceRoot: projectRoot,
+    targets: {
+      lint: {
+        executor: '@quip/nx-go:lint',
+      },
+    },
+  });
+  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, {
+    packageName: names(options.name).fileName.split('-').join('_'),
+    template: '',
+  });
+  await formatFiles(tree);
+}
+
+export default libraryGenerator;
